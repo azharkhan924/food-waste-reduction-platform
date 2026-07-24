69e39c127a0c7c8bc4ac41301895d2c8e1892b79
Azhar Khan <azharkhan230826@acropolis.in>
2026-07-24 12:44:46 +0530

    feat: create login and register UI with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
 }
-export default App
+
+export default App;
