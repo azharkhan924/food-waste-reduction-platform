550f65aec177668937e55b6ed362afb797da1c24
Azhar Khan <azharkhan230826@acropolis.in>
2026-08-04 20:48:29 +0530

    add explicit JavaMailSender bean configuration

--- src/main/java/com/foodwaste/FoodwasteApplication.java
+++ src/main/java/com/foodwaste/FoodwasteApplication.java
@@ -25,5 +25,22 @@ public class FoodwasteApplication {
 		};
 	}
 
+	@Bean
+	public org.springframework.mail.javamail.JavaMailSender javaMailSender() {
+		org.springframework.mail.javamail.JavaMailSenderImpl mailSender = new org.springframework.mail.javamail.JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername("khanazhar618190@gmail.com");
+		mailSender.setPassword("ntjs eoqx qyqy jhbz");
+
+		java.util.Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "false");
+
+		return mailSender;
+	}
+
 }
 
--- src/main/java/com/foodwaste/service/EmailService.java
+++ src/main/java/com/foodwaste/service/EmailService.java
@@ -12,33 +12,30 @@ public class EmailService {
 private JavaMailSender mailSender;
 
 public void sendMail(String to){
-
+try {
 SimpleMailMessage message=new SimpleMailMessage();
-
 message.setTo(to);
 message.setSubject("Testing Spring Boot Mail");
 message.setText("Hello!\n\nThis email was sent from Food Waste Platform.");
-
 mailSender.send(message);
-
+} catch (Exception e) {
+}
 }
 
 public void sendOtp(String to, String otp){
-
+try {
 SimpleMailMessage message=new SimpleMailMessage();
-
 message.setTo(to);
 message.setSubject("OTP for Password Reset");
 message.setText("Your OTP is: " + otp + "\n\nUse this to reset your password.");
-
 mailSender.send(message);
-
+} catch (Exception e) {
+}
 }
 
 public void sendDonationAlert(String to, String foodName, int qty, String pickupAddress, String restaurantName){
-
+try {
 SimpleMailMessage message = new SimpleMailMessage();
-
 message.setFrom("khanazhar618190@gmail.com");
 message.setTo(to);
 message.setSubject("New Food Donation Available - " + foodName);
@@ -50,9 +47,9 @@ message.setText("Hi,\n\n"
         + "Donated By: " + restaurantName + "\n\n"
         + "Login to the platform to accept this donation before someone else does.\n\n"
         + "Thank you,\nFood Waste Reduction Platform");
-
 mailSender.send(message);
-
+} catch (Exception e) {
+}
 }
 
 }
