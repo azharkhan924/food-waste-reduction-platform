c121092b83d9eabf11fc08300b9d0d194ab16434
Azhar Khan <azharkhan230826@acropolis.in>
2026-08-15 10:00:51 +0530

    Refactor repetitive HTML into Thymeleaf fragments

--- src/main/java/com/foodwaste/config/CorsConfig.java
+++ src/main/java/com/foodwaste/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500")
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
